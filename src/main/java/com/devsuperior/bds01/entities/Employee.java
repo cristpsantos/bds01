8353f6da5821aa905499becf2dc3853e6c5b06b0
cristpsantos <pontescr@gmail.com>
2021-12-02 22:30:48 -0300

    Refactor add serializable

--- src/main/java/com/devsuperior/bds01/entities/Employee.java
+++ src/main/java/com/devsuperior/bds01/entities/Employee.java
@@ -1,5 +1,7 @@
 package com.devsuperior.bds01.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,8 +12,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_employee")
-public class Employee {
-
+public class Employee implements Serializable {
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
